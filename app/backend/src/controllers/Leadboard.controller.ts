fb836dd0f640deba5a7508716d1555993ec282f6
Cozmu <www.jorgereis@gmail.com>
2023-04-05 21:30:28 -0300

    ...

--- app/backend/src/controllers/Leadboard.controller.ts
+++ app/backend/src/controllers/Leadboard.controller.ts
@@ -2,8 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 import ILeadboard from './interfaces/ILeadboard.controller';
 
 class Leadboard implements ILeadboard {
-  async listLeadboard(req:Request, res:Response):Promise<Response> => {
-
+  async listLeadboard(req:Request, res:Response):Promise<Response> {
+    const result = await 
   }
 }
 
