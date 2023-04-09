8aae32c437a70f730fd4fbf88ff4a65e88a73042
Cozmu <www.jorgereis@gmail.com>
2023-04-09 16:10:41 -0300

    feat: add sequelize.cast mas nao funciona

--- app/backend/src/services/Leadboard.service.ts
+++ app/backend/src/services/Leadboard.service.ts
@@ -1,5 +1,6 @@
 import { ModelStatic /* Op */ } from 'sequelize';
 import sequelize = require('sequelize');
+// import ILeadboard from '../database/models/interfaces/ILeadboard.model';
 import TeamsModel from '../database/models/Teams.model';
 import MatchesModel from '../database/models/Matchers.model';
 
@@ -10,10 +11,8 @@ class LeadboardService {
   ) {}
 
   async listHomeLeadboard():Promise<MatchesModel[]> {
-    const result = await this._matchesModel.findAll({
-      where: { inProgress: false },
-      attributes: [
-        [sequelize.literal('team_name'), 'name'],
+    const request = await this._matchesModel.findAll({ where: { inProgress: false },
+      attributes: [[sequelize.literal('team_name'), 'name'],
         [sequelize.fn('COUNT', sequelize.col('home_team_id')), 'totalGames'],
         [sequelize.literal('COUNT(CASE WHEN home_team_goals > away_team_goals THEN 1 END)'),
           'totalVictories'],
@@ -21,27 +20,25 @@ class LeadboardService {
           'totalDraws'],
         [sequelize.literal('COUNT(CASE WHEN home_team_goals < away_team_goals THEN 1 END)'),
           'totalLosses'],
-        [sequelize.fn('SUM', sequelize.col('home_team_goals')), 'goalsFavor'],
-        [sequelize.fn('SUM', sequelize.col('away_team_goals')), 'goalsOwn'],
+        [sequelize.fn('SUM', sequelize.cast(sequelize.col('home_team_goals'), 'integer')),
+          'goalsFavor'],
+        [sequelize.fn('SUM', sequelize.cast(sequelize.col('away_team_goals'), 'integer')),
+          'goalsOwn'],
         [sequelize.literal('SUM(`home_team_goals`) - SUM(`away_team_goals`)'), 'goalsBalance']],
       group: ['home_team_id'],
       include: [{ model: TeamsModel, as: 'homeTeam', attributes: [] }],
     });
-    return result;
+    return request;
   }
+
+  // const result = this.addScoreAndEfficiency(request);
+  // addScoreAndEfficiency = (request:ILeadboard[]) => {
+  //   for (let i = 0; i < request.length; i += 1) {
+  //     request[i].totalPoints = (request[i].totalVictories * 3) + request[i].totalDraws;
+  //     request[i].efficiency = ((request[i].totalPoints / (request[i].totalGames * 3)) * 100)
+  //       .toFixed(2);
+  //   }
+  // };
 }
 
 export default LeadboardService;
-
-// [sequelize.fn(
-//   'COUNT',
-//   sequelize.col('home_team_goals') > sequelize.col('away_team_goals'),
-// ), 'totalVictories'],
-// [sequelize.fn(
-//   'COUNT',
-//   sequelize.col('home_team_goals') === sequelize.col('away_team_goals'),
-// ), 'totalDraws'],
-// [sequelize.fn(
-//   'COUNT',
-//   sequelize.col('home_team_goals') < sequelize.col('away_team_goals'),
-// ), 'totalLosses'],
